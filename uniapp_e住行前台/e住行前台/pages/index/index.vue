fefe23100dd3a6fbbbaaa65fb2d9da2cc1fe9289
ISheenSomeOne <1611312042@qq.com>
2020-05-27 22:54:49 +0800

    add eLiveMove and uiniui demo

---
+++ uniapp_e住行前台/e住行前台/pages/index/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<view class="container">
+		<uni-grid :column="3" :show-border="false">
+			<uni-grid-item>
+				<view class="room">
+					301
+				</view>
+			</uni-grid-item>
+			<uni-grid-item><view class="room">
+				301
+			</view></uni-grid-item>
+		</uni-grid>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+	.room {
+		text-align: center;
+	}
+</style>
