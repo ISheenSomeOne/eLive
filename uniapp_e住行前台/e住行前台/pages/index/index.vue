bbfa8bb9cfff4a8e0429d80829986cbdfa0aef40
ISheenSomeOne <1611312042@qq.com>
2020-05-28 22:28:58 +0800

    do index page

--- uniapp_e住行前台/e住行前台/pages/index/index.vue
+++ uniapp_e住行前台/e住行前台/pages/index/index.vue
@@ -1,14 +1,54 @@
 <template>
 	<view class="container">
-		<uni-grid :column="3" :show-border="false">
+		<picker mode="selector" value="index" :range="typepikerData" @change="typeChange">
+			<view>{{typepikerData[typeIndex]}}</view>
+		</picker>
+		<uni-grid :column="3" :show-border="false" :highlight="false">
 			<uni-grid-item>
-				<view class="room">
-					301
+				<view class="room kongxian">
+					<text>301</text>
 				</view>
 			</uni-grid-item>
-			<uni-grid-item><view class="room">
-				301
-			</view></uni-grid-item>
+			<uni-grid-item>
+				<view class="room yuliu">
+					<text title="301232132132131">123123</text>
+				</view>
+			</uni-grid-item>
+			<uni-grid-item>
+				<view class="room zaizhu">
+					<text>301</text>
+				</view>
+			</uni-grid-item>
+			<uni-grid-item>
+				<view class="room chaoshi">
+					<text>301</text>
+				</view>
+			</uni-grid-item>
+			<uni-grid-item>
+				<view class="room dasao">
+					<text title="301232132132131">301232132132131</text>
+				</view>
+			</uni-grid-item>
+			<uni-grid-item>
+				<view class="room weixiu">
+					<text>301</text>
+				</view>
+			</uni-grid-item>
+			<uni-grid-item>
+				<view class="room baoliu">
+					<text>301</text>
+				</view>
+			</uni-grid-item>
+			<uni-grid-item>
+				<view class="room">
+					<text title="301232132132131">301232132132131</text>
+				</view>
+			</uni-grid-item>
+			<uni-grid-item>
+				<view class="room">
+					<text>301</text>
+				</view>
+			</uni-grid-item>
 		</uni-grid>
 	</view>
 </template>
@@ -17,22 +57,70 @@
 	export default {
 		data() {
 			return {
-				href: 'https://uniapp.dcloud.io/component/README?id=uniui'
+				href: 'https://uniapp.dcloud.io/component/README?id=uniui',
+				typepikerData: ['1楼','2楼','3楼'],
+				typeIndex: 0
 			}
 		},
 		methods: {
-
+			typeChange:function(e){
+				this.typeIndex = e.detail.value;
+			}
 		}
 	}
 </script>
 
-<style>
+<style lang="scss">
 	.container {
 		padding: 20px;
 		font-size: 14px;
 		line-height: 24px;
 	}
+
 	.room {
 		text-align: center;
+		height: 90%;
+		width: 90%;
+		background-color: #007AFF;
+		margin: 5%;
+		color: #FFFFFF;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		font-weight: bold;
+	}
+
+	.room text {
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+	
+	.kongxian{
+		background-color: $uni-color-success;
+	}
+	
+	.yuliu{
+		background-color: #54d2ff;
+	}
+	
+	.zaizhu{
+		background-color: $uni-color-primary;
+	}
+	
+	.chaoshi{
+		background-color: $uni-color-error;
+	}
+	
+	.dasao{
+		background-color: #999999;
+	}
+	
+	.weixiu{
+		background-color: $uni-color-warning;
+	}
+	
+	.baoliu{
+		background-color: #343a40;
 	}
 </style>
