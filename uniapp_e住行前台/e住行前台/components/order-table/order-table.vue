08f1b47d7963d042e1007592cdacd935e9eaeb0e
xjq <1611312042@qq.com>
2020-06-03 18:17:40 +0800

    add order page

---
+++ uniapp_e住行前台/e住行前台/components/order-table/order-table.vue
@@ -0,0 +1,61 @@
+<template>
+	<view class="table-box">
+		<view class="table-list-item table-title">
+			<view class="table-cont-item">来源</view>
+			<view class="table-cont-item">姓名</view>
+			<view class="table-cont-item">房间信息</view>
+			<view class="table-cont-item">入离日期</view>
+			<view class="table-cont-item">房价</view>
+		</view>
+		<view class="table-list-item">
+			<view class="table-cont-item">美团</view>
+			<view class="table-cont-item">张三</view>
+			<view class="table-cont-item">1间 智享大床房</view>
+			<view class="table-cont-item">05-26至05-27</view>
+			<view class="table-cont-item">100</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'OrderTable',
+	props: {
+		type: {
+			type: String,
+			default: 'default'
+		},
+		inverted: {
+			type: Boolean,
+			default: false
+		},
+		text: {
+			type: [String, Number],
+			default: ''
+		},
+		size: {
+			type: String,
+			default: 'normal'
+		}
+	},
+	data() {
+		return {
+			badgeStyle: ''
+		};
+	},
+	mounted() {},
+	methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.table-box {
+	font-size: 14px;
+}
+
+.table-cont-item {
+}
+
+.table-title {
+}
+</style>
--- uniapp_e住行前台/e住行前台/pages/order/order.vue
+++ uniapp_e住行前台/e住行前台/pages/order/order.vue
@@ -1,22 +1,100 @@
 <template>
-	<view>
-		
+	<view style="height: 100%;">
+		<view class="uni-tab-bar">
+			<scroll-view scroll-x="true" class="uni-swiper-tab">
+				<view class="swiper-list-box">
+					<block v-for="(tabBar, index) in tabBars" :key="index">
+						<view class="swiper-tab-list" :class="{ active: tabIndex == index }" @tap="toggleTab(index)">
+							{{ tabBar.name }}
+							<view class="swiper-tab-line"></view>
+						</view>
+					</block>
+				</view>
+			</scroll-view>
+		</view>
+
+		<!--内容区-->
+		<view class="uni-tab-bar" style="height: 100%;background-color: #54D2FF;">
+			<swiper :current="tabIndex" @change="tabChange">
+				<swiper-item>
+					<order-table></order-table>
+				</swiper-item>
+				<swiper-item>2</swiper-item>
+				<swiper-item>3</swiper-item>
+				<swiper-item>4</swiper-item>
+			</swiper>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
+export default {
+	data() {
+		return {
+			tabIndex: 0, //选中标签栏的序列
+			contentList: ['待入住', '入住中', '全部', '创建订单'],
+			tabBars: [
+				{
+					name: '待入住',
+					id: '1'
+				},
+				{
+					name: '入住中',
+					id: '2'
+				},
+				{
+					name: '全部',
+					id: '3'
+				},
+				{
+					name: '创建订单',
+					id: '4'
+				}
+			],
+			swiperHeight: 0
+		};
+	},
+	methods: {
+		toggleTab(index) {
+			// console.log(index);
+			this.tabIndex = index;
 		},
-		methods: {
-			
+		//滑动切换swiper
+		tabChange(e) {
+			// console.log(e.detail);
+			const tabIndex = e.detail.current;
+			this.tabIndex = tabIndex;
 		}
 	}
+};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.swiper-list-box {
+	display: flex;
+	justify-content: space-around;
+	padding: 5px;
+	height: 34px;
+	line-height: 34px;
+}
+.swiper-tab-list {
+	color: #FFFFFF;
+	width: 177rpx;
+	font-size: 14px;
+}
+.uni-tab-bar .active {
+	color: #FFF;
+	font-weight: bold;
+}
 
+.active .swiper-tab-line {
+	border-bottom: 3px solid #FFF;
+	width: 80rpx;
+	margin: auto;
+	// border-top: 2rpx solid #FFF;
+	border-radius: 10px;
+}
+.uni-swiper-tab {
+	background-color: $uni-color-primary;
+}
 </style>
